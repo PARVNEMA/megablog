89bf67ecd2d124b75cbfe0cb28419659ff87e157
Parv nema <namannema0704@gmail.com>
2024-03-28 09:07:21 +0530

    post updated

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -56,9 +56,9 @@ export default function Post() {
           )}
         </div>
         <div className="w-full mb-6">
-          <h1 className="text-2xl font-bold">{post.title}</h1>
+          <h1 className="text-2xl font-bold text-white">{post.title}</h1>
         </div>
-        <div className="browser-css">{parse(post.content)}</div>
+        <div className="browser-css text-white">{parse(post.content)}</div>
       </Container>
     </div>
   ) : null;
